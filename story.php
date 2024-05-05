7f3de730b0fb79f9c8140e80c5545414e5d1deec
ThanukaPerera <thanukalap@gmail.com>
2024-05-05 20:40:50 +0530

    story and contact

---
+++ story.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Our Story</title>
+  <link rel="stylesheet" href="./css/OurStory.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Poppins:wght@200;300;400&display=swap');
+  </style>
+</head>
+
+<body>
+<?php
+require_once 'navbar.php';
+?>
+  <div>
+    <div class="MainContainer">
+      <div class="container1">
+        <h1 class="heading">
+          Welcome to Sprinkle Heaven 
+        </h1>
+        <p class="info text">A heaven for doughnut enthusiasts and sweet treat lovers alike! Nestled in the heart of Colombo, our doughnut shop is a labor of love, dedicated to crafting the most delicious and delightful doughnuts you'll ever taste.
+          <br> <br>
+          Our journey began with a simple goal: to create a doughnut experience like no other. We start each day before the sun rises, handcrafting each doughnut with care and attention to detail. From our classic glazed rings to our decadent filled varieties, every doughnut is made using only the finest ingredients, ensuring a taste that's nothing short of perfection.
+          <br> <br>
+          But we're not just about doughnuts; we're about creating moments of happiness. Whether you're stopping by for your morning coffee and a quick bite, or you're treating yourself to a box of assorted flavors to share with friends and family, we want every visit to our shop to be a memorable one.
+          <br> <br>
+          We believe that doughnuts should be more than just a sweet treat; they should be a celebration of life's simple pleasures. That's why we're constantly innovating and experimenting with new flavors and creations, always striving to bring you something new and exciting to tickle your taste buds.
+          <br> <br>
+          So come on in, take a seat, and indulge in the delicious world of doughnuts. Join us in celebrating the joy of good food, good company, and good times. We can't wait to share our passion for doughnuts with you!
+        </p>
+      </div>
+      <div class="container2">
+        <img class="img1" src="./Images/Dough-nuts.jpg" alt="DoughnutImage">
+      </div>
+    </div>
+  </div>
+<?php
+require_once 'footer.php';
+?>
+</body>
+
+</html>
